36b69206cb21e39e98f8262e3e2f4493e6077feb
Richard Dennis <richard@dragoninfosec.com>
2017-09-11 14:24:28 +0100

    Update consensus_strip.py

--- consensus_strip.py
+++ consensus_strip.py
@@ -6,6 +6,7 @@ f = open("relays.txt", "w+")
 for line in urllib2.urlopen("http://86.59.21.38/tor/status-vote/current/consensus.txt"):
     ipAddress = re.findall(r"[0-9]+(?:\.[0-9]+){3}", line)
     #print ipAddress
-    print type(ipAddress)
+    #print type(ipAddress)
     f.write(str(ipAddress))
+    f.write("\n")
 f.close()
